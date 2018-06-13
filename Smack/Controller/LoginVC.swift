96904c5b05e5fb7344c177d504a27cdaf40d0111
Juan Bernardo Cruz Ortiz <Bernardo@macs-MacBook-Pro.local>
2018-06-12 19:59:46 -0500

    Lesson 04 - Create a Login UI

---
+++ Smack/Controller/LoginVC.swift
@@ -0,0 +1,23 @@
+//
+//  LoginVCViewController.swift
+//  Smack
+//
+//  Created by Juan Bernardo Cruz Ortiz on 6/12/18.
+//  Copyright © 2018 Juan Bernardo Cruz Ortiz. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func closePressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+}
---
+++ Smack/Utilities/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  Smack
+//
+//  Created by Juan Bernardo Cruz Ortiz on 6/12/18.
+//  Copyright © 2018 Juan Bernardo Cruz Ortiz. All rights reserved.
+//
+
+import Foundation
+
+//Segues
+let TO_LOGIN = "toLogin"
