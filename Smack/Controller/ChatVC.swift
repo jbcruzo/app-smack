a53c46ec8e1d42e1d8742bb9bd4b757841631cb9
Juan Bernardo Cruz Ortiz <Bernardo@macs-MacBook-Pro.local>
2018-06-22 15:27:27 -0500

    lesson 18 - Loggin Users servicios de busqueda de usarios, validación de registros de cambios y actualizacion de objetos con info de user logeado

--- Smack/Controller/ChatVC.swift
+++ Smack/Controller/ChatVC.swift
@@ -17,8 +17,14 @@ class ChatVC: UIViewController {
         super.viewDidLoad()
         menuBtn.addTarget(self.revealViewController(), action: #selector(SWRevealViewController.revealToggle(_:)), for: .touchUpInside)
         self.view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
-        
         self.view.addGestureRecognizer(self.revealViewController().tapGestureRecognizer())
+        
+        //Estas condiciones son para que verifique si hay un usuario logeado y si lo hay pedir al centtro de notificaciones c
+        if AuthService.instance.isLoggedIn {
+            AuthService.instance.findUserByEmail(completion: { (sucess) in
+                NotificationCenter.default.post(name: NOTIF_USER_DATA_DID_CHANGE, object: nil)
+            })
+        }
     }
 }
 
